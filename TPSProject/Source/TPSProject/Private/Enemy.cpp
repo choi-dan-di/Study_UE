b84cdf6e0a99bd09bdf7629fcdd19bd68c69d2a1
choi-dan-di <tanvely@kakao.com>
2023-03-17 17:00:24 +0900

    적 캐릭터 스켈레탈 메시 변경, ABP 추가

--- TPSProject/Source/TPSProject/Private/Enemy.cpp
+++ TPSProject/Source/TPSProject/Private/Enemy.cpp
@@ -11,7 +11,7 @@ AEnemy::AEnemy()
 	PrimaryActorTick.bCanEverTick = true;
 
 	// 1. 스켈레탈 메시 데이터 로드
-	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'"));
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Enemy/Model/vampire_a_lusth.vampire_a_lusth'"));
 	// 1-1. 데이터 로드 성공하면
 	if (tempMesh.Succeeded())
 	{
@@ -19,10 +19,17 @@ AEnemy::AEnemy()
 		GetMesh()->SetSkeletalMesh(tempMesh.Object);
 		// 1-3. 메시 위치 및 회전 설정
 		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -88), FRotator(0, -90, 0));
+		// 1-4. 메시 크기 수정
+		GetMesh()->SetRelativeScale3D(FVector(0.84f));
 	}
 
 	// EnemyFSM 컴포넌트 추가
 	fsm = CreateDefaultSubobject<UEnemyFSM>(TEXT("FSM"));
+
+	// 애니메이션 블루프린트 할당하기
+	ConstructorHelpers::FClassFinder<UAnimInstance> tempClass(TEXT("/Script/Engine.AnimBlueprint'/Game/Blueprints/ABP_Enemy.ABP_Enemy_C'"));
+	if (tempClass.Succeeded())
+		GetMesh()->SetAnimInstanceClass(tempClass.Class);
 }
 
 // Called when the game starts or when spawned
